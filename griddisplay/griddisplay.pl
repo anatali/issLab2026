45c3af7e32042b221c34aa1740cfd3745c2892e0
Antonio Natali <antonio.natali@unibo.it>
2026-04-29 09:46:40 +0200

    April29

--- griddisplay/griddisplay.pl
+++ griddisplay/griddisplay.pl
@@ -1,9 +1,8 @@
 %====================================================================================
 % griddisplay description   
 %====================================================================================
-mqttBroker("localhost", "1883", "grid").
-dispatch( cellstate, cellstate(X,Y,COLOR) ). %commute cell state
-event( cellstate, cellstate(X,Y,COLOR) ). %commute cell state
+dispatch( cellstate, cellstate(X,Y,COLOR) ).
+event( cellstate, cellstate(X,Y,COLOR) ).
 %====================================================================================
 context(ctxgrid, "localhost",  "TCP", "8050").
  qactor( griddisplay, ctxgrid, "it.unibo.griddisplay.Griddisplay").
