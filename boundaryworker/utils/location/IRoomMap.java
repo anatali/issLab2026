1a2eaf5e885b6044227a29ac3894f9903d10365b
Antonio Natali <antonio.natali@unibo.it>
2026-05-19 08:50:51 +0200

    May19

--- boundaryworker/utils/location/IRoomMap.java
+++ boundaryworker/utils/location/IRoomMap.java
@@ -1,24 +1,46 @@
-package location;
+ 
 
-import location.RoomMap.cellvalue;
-
-public interface IRoomMap {
+ 
 	/*
 	 *  0 : UNKNOWN
-	 *  1 : obstacle
-	 *  2 : free
-	 *  3 : robot
+	 *  1 : free
+	 *  X : obstacle 
+	 *  r : robot
 	 */
-	 int getNr();
-	 int getNc();
-	 void clear();
-	 boolean typeOfCell( int x, int y, cellvalue v );
-	 void setCell( int x, int y, cellvalue v );
-	 void setCellClean( int x, int y  );
-	 void setRobot( int x, int y  );
-	 void setFree( int x, int y  );
-	 void setObstacle( int x, int y  ) ;
-	 String toProlog();
-	 void showMap();
-	 void setPos(int x, int y, cellvalue v);
+package location;
+	import java.io.Serializable;
+	import location.RoomMap.cellvalue;
+	import location.RoomMap.Direction;
+ 
+	/**
+	* Interfaccia IRoomMap
+	* Definisce i metodi per l'interazione con una mappa bidimensionale.
+	* 
+	* Estende Serializable per garantire che ogni implementazione possa 
+	* essere trasmessa via rete o salvata in formato binario.
+	*/
+	public interface IRoomMap extends Serializable {
+	// Gestione Robot
+	void setRobotAtHome();
+	void setRobotPos(int x, int y);
+	void setRobotPos(int x, int y, Direction dir, cellvalue v);
+	Direction getDir();
+	void setDir(Direction dir);
+	// Movimento Logico
+	void doStep();
+	void turnLeft();
+	void turnRight();
+	// Gestione Celle
+	void setCell(int x, int y, cellvalue v);
+	void setObstacle(int x, int y);
+	void setFree(int x, int y);
+	void clear();
+	boolean typeOfCell(int x, int y, cellvalue v);
+	// Dimensioni
+	int getNr();
+	int getNc();
+	// Export e Persistenza
+	String toProlog();
+	void saveRoomMap(String fname) throws Exception;
+	void showMap();	
 }
